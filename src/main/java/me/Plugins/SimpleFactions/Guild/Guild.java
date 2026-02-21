f0c3d7fd5c0a2f71f3283af49d909f7c818a9aaf
Drefvelin <vebjorld@stud.ntnu.no>
2026-02-21 14:11:09 +0100

    guild eviction

--- src/main/java/me/Plugins/SimpleFactions/Guild/Guild.java
+++ src/main/java/me/Plugins/SimpleFactions/Guild/Guild.java
@@ -648,6 +648,22 @@ public class Guild {
         return 25+Math.pow(cost, 1.1);
     }
 
+    public double getEvictionCost() {
+        return getElevationCost()*2;
+    }
+
+    public boolean canBeEvicted(Player p) {
+        if(isBase()) {
+            if(p != null) p.sendMessage("§cCannot evict the main guild");
+            return false;
+        }
+        if(host.getGovernment().getMovementByMember(leader) != null) {
+            if(p != null) p.sendMessage("§cGuild is currently involved in a movement");
+            return false;
+        }
+        return true;
+    }
+
     public boolean canBeElevated(Player p) {
         if(isBase()) {
             if(p != null) p.sendMessage("§cCannot elevate the main guild");
